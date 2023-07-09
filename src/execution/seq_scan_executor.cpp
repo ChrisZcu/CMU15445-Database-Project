930afdc799c85bd30ac7bdd7766ff224de1f0e38
zcuzhang <zcuzhang@tencent.com>
2023-07-09 23:39:50 +0800

    project3 init

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -12,12 +12,13 @@
 
 #include "execution/executors/seq_scan_executor.h"
 
-namespace bustub {
+namespace bustub
+{
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+    SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+    void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+    auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
 
-}  // namespace bustub
+} // namespace bustub
