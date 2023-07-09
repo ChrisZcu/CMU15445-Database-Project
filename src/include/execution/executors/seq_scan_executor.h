03e6d594e37670b10cad72e74af6d650e6631211
zcuzhang <zcuzhang@tencent.com>
2023-07-09 23:37:27 +0800

    project3: init

--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -19,36 +19,40 @@
 #include "execution/plans/seq_scan_plan.h"
 #include "storage/table/tuple.h"
 
-namespace bustub {
-
-/**
- * The SeqScanExecutor executor executes a sequential table scan.
- */
-class SeqScanExecutor : public AbstractExecutor {
- public:
-  /**
-   * Construct a new SeqScanExecutor instance.
-   * @param exec_ctx The executor context
-   * @param plan The sequential scan plan to be executed
-   */
-  SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan);
-
-  /** Initialize the sequential scan */
-  void Init() override;
+namespace bustub
+{
 
   /**
-   * Yield the next tuple from the sequential scan.
-   * @param[out] tuple The next tuple produced by the scan
-   * @param[out] rid The next tuple RID produced by the scan
-   * @return `true` if a tuple was produced, `false` if there are no more tuples
+   * The SeqScanExecutor executor executes a sequential table scan.
    */
-  auto Next(Tuple *tuple, RID *rid) -> bool override;
+  class SeqScanExecutor : public AbstractExecutor
+  {
+  public:
+    /**
+     * Construct a new SeqScanExecutor instance.
+     * @param exec_ctx The executor context
+     * @param plan The sequential scan plan to be executed
+     */
+    SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan);
 
-  /** @return The output schema for the sequential scan */
-  auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); }
+    /** Initialize the sequential scan */
+    void Init() override;
 
- private:
-  /** The sequential scan plan node to be executed */
-  const SeqScanPlanNode *plan_;
-};
-}  // namespace bustub
+    /**
+     * Yield the next tuple from the sequential scan.
+     * @param[out] tuple The next tuple produced by the scan
+     * @param[out] rid The next tuple RID produced by the scan
+     * @return `true` if a tuple was produced, `false` if there are no more tuples
+     */
+    auto Next(Tuple *tuple, RID *rid) -> bool override;
+
+    /** @return The output schema for the sequential scan */
+    auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); }
+
+  private:
+    /** The sequential scan plan node to be executed */
+    const SeqScanPlanNode *plan_;
+    TableHeap *table_;
+    std::unique_ptr<TableIterator> iterator;
+  };
+} // namespace bustub
