47d5ee5fb0878d2f34e1b9e3bae8ec7791efe977
chriszcu <1164806828@qq.com>
2023-07-10 23:22:51 +0800

    project3: sort

--- src/execution/sort_executor.cpp
+++ src/execution/sort_executor.cpp
@@ -1,13 +1,63 @@
 #include "execution/executors/sort_executor.h"
 
-namespace bustub {
+namespace bustub
+{
 
-SortExecutor::SortExecutor(ExecutorContext *exec_ctx, const SortPlanNode *plan,
-                           std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    SortExecutor::SortExecutor(ExecutorContext *exec_ctx, const SortPlanNode *plan,
+                               std::unique_ptr<AbstractExecutor> &&child_executor)
+        : AbstractExecutor(exec_ctx), child_(std::move(child_executor)), plan_(plan)
+    {
+    }
 
-void SortExecutor::Init() { throw NotImplementedException("SortExecutor is not implemented"); }
+    void SortExecutor::Init()
+    {
+        child_->Init();
+        Tuple tuple{};
+        RID rid;
+        while (child_->Next(&tuple, &rid))
+        {
+            tuples_.push_back(tuple);
+        }
+        // 排序
+        // 比较器：[作用域内的参数](入参)
+        std::sort(tuples_.begin(), tuples_.end(),
+                  [order_bys = plan_->order_bys_, schema = child_->GetOutputSchema()](const Tuple &tuple_a, const Tuple &tuple_b)
+                  {
+                      for (const auto &order_key : order_bys)
+                      {
+                          switch (order_key.first)
+                          {
+                          case OrderByType::INVALID:
+                          case OrderByType::DEFAULT:
+                          case OrderByType::ASC:
+                              if (static_cast<bool>(order_key.second->Evaluate(&tuple_a, schema).CompareLessThan(order_key.second->Evaluate(&tuple_b, schema))))
+                                  return true;
+                              else
+                                  return false;
+                              break;
+                          case OrderByType::DESC:
+                              if (static_cast<bool>(order_key.second->Evaluate(&tuple_a, schema).CompareLessThan(order_key.second->Evaluate(&tuple_b, schema))))
+                                  return false;
+                              else
+                                  return true;
+                              break;
+                          default:
+                              break;
+                          }
+                      }
+                      return false;
+                  });
+        iter_ = tuples_.begin();
+    }
 
-auto SortExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+    auto SortExecutor::Next(Tuple *tuple, RID *rid) -> bool
+    {
+        if (iter_ == tuples_.end())
+            return false;
+        *tuple = *iter_;
+        *rid = (*tuple)->GetRid(); //???
+        ++iter_;
+        return true;
+    }
 
-}  // namespace bustub
+} // namespace bustub
--- src/include/execution/executors/sort_executor.h
+++ src/include/execution/executors/sort_executor.h
@@ -21,36 +21,41 @@
 #include "execution/plans/sort_plan.h"
 #include "storage/table/tuple.h"
 
-namespace bustub {
-
-/**
- * The SortExecutor executor executes a sort.
- */
-class SortExecutor : public AbstractExecutor {
- public:
-  /**
-   * Construct a new SortExecutor instance.
-   * @param exec_ctx The executor context
-   * @param plan The sort plan to be executed
-   */
-  SortExecutor(ExecutorContext *exec_ctx, const SortPlanNode *plan, std::unique_ptr<AbstractExecutor> &&child_executor);
-
-  /** Initialize the sort */
-  void Init() override;
+namespace bustub
+{
 
   /**
-   * Yield the next tuple from the sort.
-   * @param[out] tuple The next tuple produced by the sort
-   * @param[out] rid The next tuple RID produced by the sort
-   * @return `true` if a tuple was produced, `false` if there are no more tuples
+   * The SortExecutor executor executes a sort.
    */
-  auto Next(Tuple *tuple, RID *rid) -> bool override;
+  class SortExecutor : public AbstractExecutor
+  {
+  public:
+    /**
+     * Construct a new SortExecutor instance.
+     * @param exec_ctx The executor context
+     * @param plan The sort plan to be executed
+     */
+    SortExecutor(ExecutorContext *exec_ctx, const SortPlanNode *plan, std::unique_ptr<AbstractExecutor> &&child_executor);
 
-  /** @return The output schema for the sort */
-  auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); }
+    /** Initialize the sort */
+    void Init() override;
 
- private:
-  /** The sort plan node to be executed */
-  const SortPlanNode *plan_;
-};
-}  // namespace bustub
+    /**
+     * Yield the next tuple from the sort.
+     * @param[out] tuple The next tuple produced by the sort
+     * @param[out] rid The next tuple RID produced by the sort
+     * @return `true` if a tuple was produced, `false` if there are no more tuples
+     */
+    auto Next(Tuple *tuple, RID *rid) -> bool override;
+
+    /** @return The output schema for the sort */
+    auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); }
+
+  private:
+    /** The sort plan node to be executed */
+    const SortPlanNode *plan_;
+    std::unique_ptr<AbstractExecutor> child_;
+    std::vector<Tuple> tuples_;
+    std::vector<Tuple>::iterator iter_;
+  };
+} // namespace bustub
