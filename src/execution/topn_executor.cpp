9ea4d492d8eb86a26f90d1c9a2a52b21200968fc
chriszcu <1164806828@qq.com>
2023-07-10 23:59:15 +0800

    project3:topN

--- src/execution/topn_executor.cpp
+++ src/execution/topn_executor.cpp
@@ -1,13 +1,67 @@
 #include "execution/executors/topn_executor.h"
 
-namespace bustub {
+namespace bustub
+{
 
-TopNExecutor::TopNExecutor(ExecutorContext *exec_ctx, const TopNPlanNode *plan,
-                           std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    TopNExecutor::TopNExecutor(ExecutorContext *exec_ctx, const TopNPlanNode *plan,
+                               std::unique_ptr<AbstractExecutor> &&child_executor)
+        : AbstractExecutor(exec_ctx), plan_(plan), child_(std::move(child_executor)) {}
 
-void TopNExecutor::Init() { throw NotImplementedException("TopNExecutor is not implemented"); }
+    void TopNExecutor::Init()
+    {
+        child_->Init();
+        // 比较器
+        auto cmp = [order_bys = plan_->order_bys_, schema = child_->GetOutputSchema()](const Tuple *tuple_a, const Tuple *tuple_b)
+        {
+            for (auto &order_by : order_bys)
+            {
+                switch (order_by)
+                {
+                case OrderByType::INVALID:
+                case OrderByType::DEFAULT:
+                case OrderByType::ASC:
+                    if (static_cast<bool>(order_by.second->Evaluate(&tuple_a, schema).CompareLessThan(order_by.second->Evaluate(&tuple_b, schema))))
+                        return true;
+                    else
+                        return false;
+                    break;
+                case OrderByType::DESC:
+                    if (static_cast<bool>(order_by.second->Evaluate(&tuple_a, schema).CompareLessThan(order_by.second->Evaluate(&tuple_b, schema))))
+                        return false;
+                    else
+                        return true;
+                    break;
 
-auto TopNExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+                default:
+                    break;
+                }
+            }
+            return false;
+        };
+        Tuple tuple{};
+        RID rid{};
+        std::priority_queue<Tuple, std::vector<Tuple>, decltype(cmp)> pq(cmp);
+        while (child_->Next(&tuple, &rid))
+        {
+            pq.push(tuple);
+            if (pq.size() > plan_->GetN())
+                pq.pop();
+        }
+        while (!pq.empty())
+        {
+            tuples_stack_.push(pq.top());
+            pq.pop();
+        }
+    }
 
-}  // namespace bustub
+    auto TopNExecutor::Next(Tuple *tuple, RID *rid) -> bool
+    {
+        if (tuples_stack_.empty())
+            return false;
+        *tuple = tuples_stack_.top();
+        *rid = tuple->GetRid();
+        tuples_stack_.pop();
+        return true;
+    }
+
+} // namespace bustub
--- src/include/execution/executors/topn_executor.h
+++ src/include/execution/executors/topn_executor.h
@@ -13,6 +13,7 @@
 #pragma once
 
 #include <memory>
+#include <stack>
 #include <vector>
 
 #include "execution/executor_context.h"
@@ -21,36 +22,40 @@
 #include "execution/plans/topn_plan.h"
 #include "storage/table/tuple.h"
 
-namespace bustub {
-
-/**
- * The TopNExecutor executor executes a topn.
- */
-class TopNExecutor : public AbstractExecutor {
- public:
-  /**
-   * Construct a new TopNExecutor instance.
-   * @param exec_ctx The executor context
-   * @param plan The topn plan to be executed
-   */
-  TopNExecutor(ExecutorContext *exec_ctx, const TopNPlanNode *plan, std::unique_ptr<AbstractExecutor> &&child_executor);
-
-  /** Initialize the topn */
-  void Init() override;
+namespace bustub
+{
 
   /**
-   * Yield the next tuple from the topn.
-   * @param[out] tuple The next tuple produced by the topn
-   * @param[out] rid The next tuple RID produced by the topn
-   * @return `true` if a tuple was produced, `false` if there are no more tuples
+   * The TopNExecutor executor executes a topn.
    */
-  auto Next(Tuple *tuple, RID *rid) -> bool override;
+  class TopNExecutor : public AbstractExecutor
+  {
+  public:
+    /**
+     * Construct a new TopNExecutor instance.
+     * @param exec_ctx The executor context
+     * @param plan The topn plan to be executed
+     */
+    TopNExecutor(ExecutorContext *exec_ctx, const TopNPlanNode *plan, std::unique_ptr<AbstractExecutor> &&child_executor);
 
-  /** @return The output schema for the topn */
-  auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); }
+    /** Initialize the topn */
+    void Init() override;
 
- private:
-  /** The topn plan node to be executed */
-  const TopNPlanNode *plan_;
-};
-}  // namespace bustub
+    /**
+     * Yield the next tuple from the topn.
+     * @param[out] tuple The next tuple produced by the topn
+     * @param[out] rid The next tuple RID produced by the topn
+     * @return `true` if a tuple was produced, `false` if there are no more tuples
+     */
+    auto Next(Tuple *tuple, RID *rid) -> bool override;
+
+    /** @return The output schema for the topn */
+    auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); }
+
+  private:
+    /** The topn plan node to be executed */
+    const TopNPlanNode *plan_;
+    std::unique_ptr<AbstractExecutor> child_;
+    std::stack<Tuple> tuples_stack_;
+  };
+} // namespace bustub
